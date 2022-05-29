187d8d2af8751046001880b60e8dcfd319c0d833
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-05-29 12:16:35 +0100

    Finished the design of the home-components, will be moving on to the base components

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,15 @@
+import { Link } from 'react-router-dom'
+import './Home.scss'
 
 export const Home = () => {
     return (
-        <div>Home</div>
+        <div className='HomeP'>
+            <div className="">Welcome to pizzarous!</div>
+            <div className="">
+                <button>
+                    <Link to="/base">Create your pizza</Link>
+                </button>
+            </div>
+        </div>
     )
 }
