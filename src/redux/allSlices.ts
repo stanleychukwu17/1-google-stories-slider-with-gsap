25e7a5ba4e8cbf661f22ae29b09a77bc6d795ba0
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-06-02 11:44:16 +0100

    started working on the redux structure - still in the planing phase

---
+++ src/redux/allSlices.ts
@@ -0,0 +1,10 @@
+const initialState = {
+    base: ['Classic', 'Thin & Crispy', 'Thick Crust'],
+    toppings: ['Mushroom', 'Pepper', 'Onions', 'Olives', 'Extra Cheese', 'Tomatoes']
+}
+
+const allSlices = {
+
+}
+
+export default allSlices
