866a9aec0cdce96252ab13ab46b08e8f380ea111
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-21 20:57:07 +0100

    updated the project index title Brought in the skeleton for what we are doing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,32 @@
-
 import './app.scss';
+// import { motion } from 'framer-motion';
+// import { gsap } from 'gsap';
 
+// import MiddlePhone from '../../components/App/md/MiddlePhone'
+
+
+// importing of all assets
+// import phone from '../../assets/svg/p1.svg'
 
 const App = () => {
 
     return (
-        <div className="App_itmCvr">
-            <div className="">New design</div>
+        <div className="AppMain">
+            <div className="AppTitle">
+                <p>Your dream</p>
+                <p>Meet the new iPhone 14 pro</p>
+            </div>
+            <div className="AppMid_1">
+                <div className="AppMgo"></div>
+                <div className="AppMgo"></div>
+                <div className='AppMgo boy'></div>
+                <div className="AppMgo"></div>
+                <div className="AppMgo"></div>
+            </div>
+            <div className="">
+                
+            </div>
         </div>
     )
 }
-
 export default App;
