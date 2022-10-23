33873f2b73f8a219969ff158b16fa28e9e97f1af
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-23 03:21:28 +0100

    smaller cards component animation completed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import './app.scss';
-import { motion } from 'framer-motion';
-// import { gsap } from 'gsap';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { gsap } from 'gsap';
 
 // importation of supporting components
 import Phone from './Phone';
@@ -14,8 +14,20 @@ import p1 from '../../assets/p1.png'
 import p2 from '../../assets/p2.png'
 import p3 from '../../assets/p3.png'
 import p4 from '../../assets/p4.png'
+import { useEffect } from 'react';
 
 const App = () => {
+    const {scrollY} = useScroll()
+    const card1Y = useTransform(scrollY, [375, 1375], [0, 1000])
+    const card1X = useTransform(scrollY, [375, 1000, 1375], [0, 0, 250])
+    const card1Xb = useTransform(scrollY, [375, 1000, 1375], [0, 0, -250])
+
+    useEffect(() => {
+        gsap.set('body', {overflow:'hidden'})
+        setTimeout(() => {
+            gsap.set('body', {overflow:'auto'})
+        }, 2500)
+    }, [])
 
     return (
         <div className="AppMain">
@@ -24,7 +36,7 @@ const App = () => {
                 <motion.p variants={t2Variant} initial="initial" animate="animate">Meet the new iPhone 14 pro</motion.p>
             </div>
             <motion.div className="AppMid_1" variants={mainCardsVariant} initial="initial" animate="animate">
-                <motion.div className="AppMgo" variants={smallCardsVariant} custom={2}>
+                <motion.div className="AppMgo" variants={smallCardsVariant} custom={2} style={{y:card1Y, x:card1X}}>
                     <div className="AppMOimg"><img src={p1} alt="" /></div>
                 </motion.div>
                 <motion.div className="AppMgo Mg2" variants={smallCardsVariant} custom={4}>
@@ -34,7 +46,7 @@ const App = () => {
                 <motion.div className="AppMgo Mg2" variants={smallCardsVariant} custom={3}>
                     <div className="AppMOimg"><img src={p3} alt="" /></div>
                 </motion.div>
-                <motion.div className="AppMgo" variants={smallCardsVariant} custom={5}>
+                <motion.div className="AppMgo" variants={smallCardsVariant} custom={5} style={{y:card1Y, x:card1Xb}}>
                     <div className="AppMOimg"><img src={p4} alt="" /></div>
                 </motion.div>
             </motion.div>
