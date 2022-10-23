abcca4101686017223a213f3b3469291cecd5a85
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-23 02:31:01 +0100

    scaled the phone downwards

--- src/components/App/Phone.tsx
+++ src/components/App/Phone.tsx
@@ -14,7 +14,7 @@ export default function Phone() {
     const oTop = 385 // the offset of the item from the top
     const end = 1000 + oTop; // the end should be after scrolling 1000px downward
     const {scrollY} = useScroll()
-    const phoneY = useTransform(scrollY, (latestY) => {
+    const phoneY = useTransform(scrollY, (latestY) => { // inCharge of pining the phone down on user scroll
         let giveBack = 0
 
         if (latestY > oTop && latestY <= end) {
@@ -26,21 +26,10 @@ export default function Phone() {
         // console.log(giveBack)
         return giveBack
     })
-
-    useEffect(() => {
-        const phoneParent = phoneRef.current.closest('div.AppMid_1') as HTMLDivElement
-        const phoneOffsetTop = phoneParent.offsetTop
-        
-        return scrollY.onChange(latest => {
-            // console.log(latest, phoneOffsetTop)
-            if (latest >= phoneOffsetTop) {
-                console.log('e don pass')
-            }
-        })
-    })
+    const scale = useTransform(scrollY, [oTop, end], [1, 0.85])
 
     return (
-        <motion.div className='AppMgo boy' variants={smallCardsVariant} custom={0} ref={phoneRef} style={{y:phoneY}}>
+        <motion.div className='AppMgo boy' variants={smallCardsVariant} custom={0} ref={phoneRef} style={{y:phoneY, scale, originY:'top'}}>
             <img src={phone} alt="" />
         </motion.div>
     )
