87fc55596ebc8c4bda19de2f690e91f177f908f1
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-28 07:43:39 +0100

    animated the title

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -21,10 +21,12 @@ import p4 from '../../assets/p4.png'
 
 const App = () => {
     const {scrollY} = useScroll()
+    const titleY = useTransform(scrollY, [0, 1000], [0, 2450])
+    const titleScale = useTransform(scrollY, [0, 1000], [1, 1.6])
     const card1Y = useTransform(scrollY, [375, 1375], [0, 1000])
     const card1X = useTransform(scrollY, [375, 1000, 1375], [0, 0, 250])
     const card1Xb = useTransform(scrollY, [375, 1000, 1375], [0, 0, -250])
-    const background = useTransform(scrollY, [900, 1275], ['#000', '#f5f6fa'])
+    const background = useTransform(scrollY, [900, 1275], ['#000', '#f5f6fa']) // changes the background color
 
     // useEffect(() => {
     //     gsap.set('body', {overflow:'hidden'})
@@ -36,7 +38,9 @@ const App = () => {
     return (
         <motion.div className="AppMain" style={{background}}>
             <div className="AppTitle">
-                <motion.p variants={t1Variant} initial="initial" animate="animate">Your dream</motion.p>
+                <motion.p variants={t1Variant} initial="initial" animate="animate" style={{y: titleY, scale: titleScale}}>
+                    Your dream
+                </motion.p>
                 <motion.p variants={t2Variant} initial="initial" animate="animate">Meet the new iPhone 14 pro</motion.p>
             </div>
             <motion.div className="AppMid_1" variants={mainCardsVariant} initial="initial" animate="animate">
